bb91e503e58a63ce269477f52e60a84750fbcb48
EC2 Default User <ec2-user@ip-172-31-56-250.ec2.internal>
2016-10-29 18:58:16 +0000

    New branch

--- cs3744/project3/sushantb/app/config.php
+++ cs3744/project3/sushantb/app/config.php
@@ -2,12 +2,12 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://localhost/cs3744/project3/sushantb/');
+define('BASE_URL','http://ec2-54-167-97-136.compute-1.amazonaws.com/cs3744/project3/sushantb/');
 
 // database constants
 define('DB_HOST','127.0.0.1');
 define('DB_USER','root');
-define('DB_PASS','');
+define('DB_PASS','admin');
 define('DB_DATABASE','bottleup');
 
 //buffer the php errors
